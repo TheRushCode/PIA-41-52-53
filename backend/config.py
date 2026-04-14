41a0bb7f4ace36e3cc565ae8fea74c23854394a3
TheRushCode <waghmarerushikesh47@gmail.com>
2026-04-14 16:50:42 +0530

    Delete backend directory

--- backend/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    MONGO_URI = "mongodb://127.0.0.1:27017/code_snippet_db"
-    JWT_SECRET_KEY = "super-secret-key"
--- backend/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_pymongo import PyMongo
-from flask_jwt_extended import JWTManager
-
-mongo = PyMongo()
-jwt = JWTManager()
-
-def init_extensions(app):
-    mongo.init_app(app)
-    jwt.init_app(app)
